4438143ee56b6a410a3d298eee8f201f6586916c
Wesley Junior <wesley_jr40@msn.com>
2019-09-18 01:31:12 -0300

    initial commit;

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const handler = (req, res) =>{    
+    console.log('Server received request.');
+    //res.status(200).send('Hello World!');
+    res.end('Hello World!');
+}
+
+const server = http.createServer(handler);
+
+server.listen(port, err=>{
+    if (err){
+        console.log(err);
+    }
+    else{
+        console.log(`Server listening on port: ${port}`);
+    }
+});
